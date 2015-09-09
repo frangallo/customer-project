2e19261fe51b3f6b65e215b097631c21a8a88302
frangallo <fgallo@me.com>
2015-09-09 16:23:59 -0700

    Finishd Great-Circle distance

--- customer-sorter.rb
+++ customer-sorter.rb
@@ -1,3 +1,40 @@
-class Users
-  def initialize(customer_list)
-    
+require 'json'
+
+class UserList
+  attr_accessor :customer_list
+  attr_reader :range
+
+  def initialize(customer_list, range)
+    @customer_list = import_list(customer_list)
+    @range = range
+  end
+
+  def import_list(customer_list)
+    File.readlines(customer_list).map!(&:chomp)
+  end
+
+  def compile_list
+    within_range = []
+    @customer_list.each do |
+
+  def distance(lat1, lon1, lat2, lon2)
+    lat1, lon1, lat2, lon2 = [lat1, lon1, lat2, lon2].map!{|el| to_radians(el)}
+
+    distance = Math::acos(
+      Math::sin(lat1)*Math::sin(lat2) +
+      Math::cos(lat1)*Math::cos(lat2) *
+      Math::cos(lon2-lon1)
+    ) * 6371
+
+    disance <= range ? true : false
+  end
+
+  private
+
+  def self.to_radians(degrees)
+   degrees * Math::PI / 180.0
+ end
+end
+
+u = UserList.new("customer.txt")
+p u.customer_list
