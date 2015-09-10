fd0d5564ca3649337413bd928bc163737dc46495
frangallo <fgallo@me.com>
2015-09-10 11:51:18 -0700

    cleaned up methods

--- customer-sorter.rb
+++ customer-sorter.rb
@@ -1,8 +1,7 @@
 require 'json'
 
 class UserList
-  attr_accessor :customer_list, :result
-  attr_reader :range, :longitude_origin_radian, :latitude_origin_radian
+  attr_reader :range, :longitude_origin, :latitude_origin, :customer_list
 
   def initialize(customer_list, range, latitude, longitude)
     @customer_list = import_list(customer_list)
@@ -12,23 +11,37 @@ class UserList
     @result = Array.new(@customer_list.length-1, nil)
   end
 
+  def compile_list
+    @customer_list.each do |user|
+      long, lat = parse_user_coords(user)
+      id = parse_user_id(user)
+      @result[id] = [parse_user_name(user), id] if distance(long,lat)
+    end
+    @result.compact
+  end
+
+  private
+
   def import_list(customer_list)
     File.readlines(customer_list).map!(&:chomp)
   end
 
-  def compile_list
-    within_range = []
-    @customer_list.each do |user|
-      long, lat = parse_user(user)
-      result[JSON.parse(user)["user_id"]] = user if distance(long,lat)
-    end
-    result.compact
+  def parse_user_name(user)
+    parse_json(user,"name")
   end
 
-  def parse_user(user)
-    long = JSON.parse(user)["longitude"].to_f
-    lat = JSON.parse(user)["latitude"].to_f
+  def parse_user_coords(user)
+    long = parse_json(user, "longitude").to_f
+    lat = parse_json(user, "latitude").to_f
     [long,lat]
+  end
+
+  def parse_user_id(user)
+    parse_json(user,"user_id")
+  end
+
+  def parse_json(user,key)
+    JSON.parse(user)[key]
   end
 
   def distance(lon, lat)
@@ -40,11 +53,10 @@ class UserList
     distance <= range ? true : false
   end
 
-
   def to_radians(degrees)
    degrees * (Math::PI / 180.0)
  end
 end
 
-u = UserList.new("customer.txt", 100, 53.3381985, 6.2592576)
-puts u.compile_list
+customer_records = UserList.new("customer.txt", 100, 53.3381985, 6.2592576)
+p customer_records.compile_list
